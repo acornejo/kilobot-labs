d7a9870597192bf3c1e9d8ec6833d6a832edbfb1
Alex Cornejo <acornejo@gmail.com>
2014-07-22 16:02:01 -0400

    change move-to-light for k-team bots

--- movetolight.c
+++ movetolight.c
@@ -1,22 +1,24 @@
 #include <kilolib.h>
 
+#define LEFT 0
+#define RIGHT 1
+
 // declare constants
-static const uint8_t THRESH_HIGH = 5;
-static const uint8_t THRESH_LOW  = 5;
+static const uint16_t THRESH_HI = 600;
+static const uint16_t THRESH_LO = 300;
 
 // declare variables
-int cur_direction = 0;
-long cur_light = 0;
-long  high_thresh = 0, low_thresh = 1024;
+uint8_t cur_direction;
+int16_t cur_light = 0;
 
 // To reduce noise we take the average of 300 samples, discarding
 // samples where the ADC sensor failed (returned -1).
 void sample_light() {
-    int numsamples = 0;
+    int16_t numsamples = 0;
     long average = 0;
 
     while (numsamples < 300) {
-        int sample = get_ambientlight();
+        int16_t sample = get_ambientlight();
         if (sample != -1) {
             average += sample;
             numsamples++;
@@ -26,41 +28,40 @@ void sample_light() {
     cur_light = average / 300;
 }
 
-void switch_directions() {
-    if (cur_direction) {
-        spinup_motors();
-        set_motors(0, kilo_turn_right);
-        set_color(RGB(2,0,0));
-    } else {
-        spinup_motors();
-        set_motors(kilo_turn_left, 0);
-        set_color(RGB(0,2,0));
-    }
-    cur_direction = !cur_direction;
+void turn_left() {
+    spinup_motors();
+    set_motors(kilo_turn_left, 0);
+    set_color(RGB(2,0,0));
 }
 
-void update_thresh() {
-    high_thresh = cur_light*(100+THRESH_HIGH)/100;
-    low_thresh = cur_light*(100-THRESH_LOW)/100;
+void turn_right() {
+    spinup_motors();
+    set_motors(0, kilo_turn_right);
+    set_color(RGB(0,2,0));
 }
 
-void compute_direction() {
-    if (cur_light < low_thresh) {
-        update_thresh();
-    } else if (cur_light > high_thresh) {
-        update_thresh();
-        switch_directions();
-        delay(300); // to avoid fast switches
-    }
-}
 
 void setup() {
-    switch_directions(); // start turning in any direction
+    cur_direction = LEFT;
+    turn_left();
 }
 
 void loop() {
     sample_light();
-    compute_direction();
+    switch(cur_direction) {
+        case LEFT:
+            if (cur_light < THRESH_LO) {
+                cur_direction = RIGHT;
+                turn_right();
+            }
+            break;;
+        case RIGHT:
+            if (cur_light > THRESH_HI) {
+                cur_direction = LEFT;
+                turn_left();
+            }
+            break;
+    }
 }
 
 int main() {
